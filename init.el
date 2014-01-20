347bc26dab6818b6808659c1a59c242c1a718417
Akihiro Uchida <uchida@turbare.net>
2014-01-20 23:20:13 +0900

    disable toolbar-mode and change linum-format

--- init.el
+++ init.el
@@ -10,7 +10,8 @@
 ;; no start up message
 (setq inhibit-startup-screen t)
 ;; hide tool-bar and menu-bar
-;;(tool-bar-mode 0)
+(when window-system
+  (tool-bar-mode 0))
 (menu-bar-mode 0)
 ;; show line and column number
 (line-number-mode t)
@@ -44,7 +45,7 @@
 (require 'linum)
 (global-linum-mode t)
 ;; dynamic linum-format
-(setq linum-format " %5d ")
+(setq linum-format "%5d ")
 
 ;; save-place
 (require 'saveplace)
