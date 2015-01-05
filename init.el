084ed5329bdb1c513623bbd501f026e8c1ebce90
Akihiro Uchida <uchida@turbare.net>
2015-01-05 16:08:13 +0900

    remove extraneous backslash in kbd

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
 ;; input special and control characters by "Option"
 (setq ns-option-modifier 'none)
 ;; enable C-w in minibuffer
-(define-key minibuffer-local-completion-map (kbd "\C-w") 'backward-kill-word)
+(define-key minibuffer-local-completion-map (kbd "C-w") 'backward-kill-word)
 ;; set recenter-position
 (setq recenter-positions '(middle))
 ;; change path to auto-save-list files
